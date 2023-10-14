f57999cc6ce4f7fa33c3329d442c80c51fbebd75
PolarisLueur <146786225+PolarisLueur@users.noreply.github.com>
2023-10-13 21:33:16 -0300

    init

---
+++ lua/core/plugins-setup.lua
@@ -0,0 +1,79 @@
+  -- use 'foo1/bar1.nvim'
+  -- use 'foo2/bar2.nvim'
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  -- insert packer plugins here
+
+  -- themes
+  use "folke/tokyonight.nvim"
+  use "catppuccin/nvim"
+  use "shaunsingh/nord.nvim"  
+    -- lua-line theme
+  use "nvim-lualine/lualine.nvim"
+  
+  -- bufferline
+  use {'akinsho/bufferline.nvim', tag = "*", requires = 'nvim-tree/nvim-web-devicons'}  
+
+  -- nvim tree for sidebar & telescope
+  use "nvim-tree/nvim-tree.lua"
+  use "nvim-tree/nvim-web-devicons"
+  use "nvim-treesitter/nvim-treesitter"
+ 
+    -- autopairs
+  use "windwp/nvim-autopairs"
+  use "windwp/nvim-ts-autotag"
+
+  -- pane movement plugin
+  use "christoomey/vim-tmux-navigator"
+  
+  -- completions
+  use "hrsh7th/nvim-cmp"
+  use "hrsh7th/cmp-buffer"
+  use "hrsh7th/cmp-path"
+  use "hrsh7th/cmp-nvim-lsp"
+
+  -- snippets
+  use "L3MON4D3/LuaSnip"
+  use "rafamadriz/friendly-snippets"
+  use "saadparwaiz1/cmp_luasnip"
+
+  -- formatter
+
+  -- lsp -> language server protocols
+  use {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+    "glepnir/lspsaga.nvim", branch = "main", -- for window like vscode
+    "jose-elias-alvarez/typescript.nvim",
+    "onsails/lspkind.nvim"
+  }
+
+  -- telescope setup, fuzzy finder
+  use {
+      "nvim-telescope/telescope.nvim",      
+      tag = "0.1.0",
+      requires = { {"nvim-lua/plenary.nvim"} }
+  }
+
+  -- end if plugins section
+    if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
---
+++ lua/core/plugins/lualine.lua
@@ -0,0 +1,15 @@
+require("lualine").setup {
+    options = {
+        icons_enabled = true,
+        theme = "nord",
+        section_separators = '', component_separators = ''
+    },
+    sections = {
+        lualine_a = {
+            {
+                "filename",
+                path = 1,
+            }
+        }
+    }
+}
